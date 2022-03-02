fb005130a48a8b2cab612245ea40ace21e46f41e
lucasdsm78 <dasilvamarqueslucas@gmail.com>
2022-03-02 19:38:18 +0100

    Call API Fix

---
+++ IOSProject/Service/Path.swift
@@ -0,0 +1,12 @@
+//
+//  Path.swift
+//  IOSProject
+//
+//  Created by admin on 02/03/2022.
+//
+
+struct Path {
+    private static let baseURL = "https://restcountries.com/v2/"
+    static let all = "\(baseURL)all"
+    static let name = "\(baseURL)name/"
+}
