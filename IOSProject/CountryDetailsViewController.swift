ff7e718baca34fde334564c8df356bbd1b3f98e3
Matteo95000 <61450585+Matteo95000@users.noreply.github.com>
2022-03-04 12:49:49 +0100

    Details

--- IOSProject/CountryDetailsViewController.swift
+++ IOSProject/CountryDetailsViewController.swift
@@ -10,42 +10,36 @@ import UIKit
 class CountryDetailsViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var countrynameLabel: UILabel!
-    @IBOutlet weak var countrynameLabel2: UILabel!
-    @IBOutlet weak var countrynameLabel3: UILabel!
     
+    @IBOutlet weak var lbCapitale: UILabel!
+    @IBOutlet weak var lbContinent: UILabel!
+    private let countriesAPICall = Service()
+    
+    @IBOutlet weak var lbNom: UILabel!
+    @IBOutlet weak var lbPopulation: UILabel!
     var country:Country?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let url = country?.flag else { return }
-        
-        imageView.downloaded(from: url)
-        countrynameLabel.text = country?.name
-        countrynameLabel2.text = country?.capital
-        countrynameLabel3.text = country?.region
-        // Do any additional setup after loading the view.
-    }
-}
+        countriesAPICall.getCountryByName(countryName: "peru", completion: {response, error in
+                    print(response)
+                    DispatchQueue.main.async {
+                        let dataTask = URLSession.shared.dataTask(with: URL(string: String(response![4]))!) { [weak self] (data, _, _) in
+                                                if let data = data {
+                                                    DispatchQueue.main.async {
+                                                        // Create Image and Update Image View
+                                                        self?.imageView.image = UIImage(data: data)
+                                                    }
+                                                }
+                                            }
 
-extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
-        contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                let data = data, error == nil,
-                let image = UIImage(data: data)
-                else { return }
-            DispatchQueue.main.async() { [weak self] in
-                self?.image = image
-            }
-        }.resume()
-    }
-    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
-        guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
+                                            // Start Data Task
+                                            dataTask.resume()
+                        self.lbContinent.text = String(response![2])
+                        self.lbCapitale.text = String(response![1])
+                        self.lbNom.text = String(response![0])
+                        self.lbPopulation.text = String(response![3])
+                    }
+                })
     }
 }
